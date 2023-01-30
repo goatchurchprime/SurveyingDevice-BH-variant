8047eded8ccf8be0c3d2f0019ac65ff1204b47bc
blhall195 <blhall195@gmail.com>
2023-01-30 19:58:40 +0000

    Display updates only when sensor values change

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,40 @@
 OLED oled; // create a OLED object
 BNO085 bno085;
 
+float new_combined_value;
+float combined_value;
+float threshold = 0.5;
+float diff;
+float distance = 0;
+
 void setup() {
   Serial.begin(9600);
   oled.Initialise();
   bno085.Initialise();
+  oled.Distance(distance);
 }
 
 void loop() {
 
-  double distance = 0;
   bool ble_status = random(0,100);
   int batt_percentage = random(0,100);
 
   float compass = bno085.Compass();
   float clino = bno085.Clino();
 
-  oled.Distance(distance);
+  combined_value = compass + clino;
+  diff = combined_value - new_combined_value;
+  
+  if (diff>threshold||diff<-threshold)  
+  {  
+  
   oled.Compass(compass);
   oled.Clino(clino);
+  float compass = bno085.Compass();
+  float clino = bno085.Clino();
+  new_combined_value = compass + clino;
+  }  
+
   oled.Blutooth(ble_status);
   oled.Battery(batt_percentage);
   delay(50);
